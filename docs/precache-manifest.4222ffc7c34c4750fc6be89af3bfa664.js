6de3884902bbcbe171db753913ae555a5ad060f7
CarliNabbe <carli_nabbe@hotmail.com>
2020-02-06 13:30:04 +0100

    Added build

---
+++ docs/precache-manifest.4222ffc7c34c4750fc6be89af3bfa664.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f01647a3ec5c5ff8a14627cccd0d69b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78620d237cb0baefdf33",
+    "url": "/static/css/main.71ee923e.chunk.css"
+  },
+  {
+    "revision": "8422ec8c3f4b0ff5a286",
+    "url": "/static/js/2.3a7914c0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.3a7914c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "78620d237cb0baefdf33",
+    "url": "/static/js/main.f9069928.chunk.js"
+  },
+  {
+    "revision": "5c532a4be03dc535857f",
+    "url": "/static/js/runtime-main.87d72c79.js"
+  }
+]);
